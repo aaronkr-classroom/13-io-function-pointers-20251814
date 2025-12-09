6e25b9bd2c0e03c98a528454213b9c0d3390ccea
ththfql <145094239+ththfql@users.noreply.github.com>
2025-12-09 15:43:22 +0900

    fin

---
+++ Week14/fprintf.c
@@ -0,0 +1,25 @@
+﻿// fprintf.c
+#include <stdio.h>
+
+nt main(void) {
+	char* string = "Hello, World!";
+	short int data = 0x0; // 16진수
+
+	int nums[6]
+
+	// 쓰기 모드로 텍스트 파일을 열기
+	FILE* p_file = fopen("nums.txt", "wt");	
+
+	if (NULL != p_file) {
+		for (int i = 412; i < 431; i++) {
+			if (i % 3 == 0) {
+				fprintf(p_file, "\n");
+			}
+			fprintf(p_file, "%d", i);
+		}
+		
+		printf("파일 내용을 이렇게 썼어요 !");
+		
+		fclose(p_file);
+	}
+}
---
+++ Week14/fwrite.c
@@ -0,0 +1,27 @@
+﻿// fwrite.c
+#include <stdio.h>
+
+int main(void) {
+	int data1 = 0x00000412,
+		data2 = 100,
+		data_list[5] = { 0, 1, 2, 3, 4 };
+	char* text = " I'm 5rabeoni! Swear to me! ";
+	FILE* p_file = fopen("jin.dat", "wb");
+
+	if (NULL != p_file) {
+		fwrite(&data1, sizeof(int), 1, p_file);
+		fwrite(&data2, sizeof(int), 5, p_file);
+
+		//fwrite(&data_list, sizeof(int), 5, p_file);
+		//fwrite(&data_list, sizeof(int),* 5, 1, p_file);
+		fwrite(&data_list sizeof(data_list), 1, p_file);
+
+		fwrite(&text, sizeof(text), 1, p_file);
+
+		printf("파일에 바이너리 데이터를 썼어요 !");'
+			
+		fclose(p_file);
+	}
+	
+	return 0;
+}
